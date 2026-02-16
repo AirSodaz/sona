33f005dd9f4f5b2d29aad727a1042508256ce782
AirSodaz <asodaz@outlook.com>
2026-02-16 15:19:41 +0800

    feat: Update Github Action Workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -250,6 +250,7 @@ async fn download_file<R: tauri::Runtime>(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .manage(DownloadState {
             downloads: Mutex::new(std::collections::HashMap::new()),
